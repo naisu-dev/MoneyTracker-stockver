db505f26e1770611467d46fb5c4799dbddb7c5b6
Taka005 <01taka005@gmail.com>
2024-01-15 08:17:46 +0900

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,27 @@
 (async()=>{
-    const data = await fetch("https://api.taka.cf/v1/money")
-      .then(res=>res.json());
+  const data = await fetch("https://api.taka.cf/v1/money")
+    .then(res=>res.json());
 
-    const money = await Promise.all(data.data
-      .sort((m1,m2)=>m2.mount - m1.mount)
-      .slice(9)
-      .map(async(money)=>{
-        const user = await fetchUser(money.id);
+  let money = data.data.sort((m1,m2)=>m2.mount - m1.mount);
 
-        return `<div class="card mb-3">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${user.avatarURL}" class="icon" height="80">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h2 class="card-title"><strong>${user.tag} - ${money.amount} 円</strong></h2>
-                    </div>
-                </div>
+  money.length = 15
+
+  money = await Promise.all(money.map(async(money)=>{
+    const user = await fetchUser(money.id);
+
+    return `<div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${user.avatarURL}" class="icon" height="80">
+          </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h2 class="card-title"><strong>${user.tag} - ${money.amount} 円</strong></h2>
+              </div>
             </div>
-        </div>`
-      }));
+        </div>
+      </div>`
+  }));
 
     const rank = document.querySelector(".rank");
 
@@ -28,8 +29,9 @@
 })();
 
 async function fetchUser(id){
-    const data = await fetch(`https://api.taka.cf/v1/discord/user?id=${id}`)
-        .then(res=>res.json());
+  const data = await fetch(`https://api.taka.cf/v1/discord/user?id=${id}`)
+    .then(res=>res.json())
+    .catch(()=>{});
 
-    return data.data;
+  return data.data;
 }
