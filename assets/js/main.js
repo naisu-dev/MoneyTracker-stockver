451327231ce3d771df25e66d4f24ed34b4da1556
Taka005 <01taka005@gmail.com>
2024-01-15 20:27:08 +0900

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 (async()=>{
-  const rank = document.querySelector(".rank");
   const next = document.getElementById("next");
 
   const data = await fetch("https://api.taka.cf/v1/money")
@@ -8,27 +7,28 @@
   data.data.sort((m1,m2)=>m2.amount - m1.amount);
 
   let count = 10;
-  const money = await getHtml(data.data,0,count);
-
-  rank.insertAdjacentHTML("beforeend",money.join(""));
+  await addRank(data.data,0,count);
 
   next.addEventListener("click",async()=>{
-    const money = await getHtml(data.data,count,count+10);
+    await addRank(data.data,count,count+10);
     count += 10;
-    rank.insertAdjacentHTML("beforeend",money.join(""));
   })
 })();
 
-async function getHtml(data,start,end){
-  return await Promise.all(data.slice(start,end).map(async(money,i)=>{
-    const user = await fetchUser(money.id);
+async function addRank(data,start,end){
+  const rank = document.querySelector(".rank");
 
-    return `<div class="card mb-3">
-      <div class="card-header"><strong>${i+start+1}位</strong></div>
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img src="${user?.avatarURL||"https://cdn.discordapp.com/embed/avatars/0.png"}" class="icon" height="80">
-          </div>
+  data.slice(start,end).forEach(async(money,i)=>{
+    const user = await fetchUser(money.id);
+    await sleep(100);
+
+    rank.insertAdjacentHTML("beforeend",
+      `<div class="card mb-3">
+        <div class="card-header"><strong>${i+start+1}位</strong></div>
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="${user?.avatarURL||"https://cdn.discordapp.com/embed/avatars/0.png"}" class="icon" height="80">
+            </div>
             <div class="col-md-8">
               <div class="card-body">
                 <h2 class="card-title"><strong>${user?.tag||"不明なユーザー"} - ${money.amount} 円</strong></h2>
@@ -36,7 +36,8 @@ async function getHtml(data,start,end){
             </div>
         </div>
       </div>`
-  }));
+    )
+  });
 }
 
 async function fetchUser(id){
@@ -45,4 +46,8 @@ async function fetchUser(id){
     .catch(()=>{});
 
   return data.data;
+}
+
+function sleep(ms){
+  return new Promise(resolve=>setTimeout(resolve,ms));
 }
